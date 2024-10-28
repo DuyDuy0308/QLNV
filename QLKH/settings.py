9cd1ce080c5ceb655c0739880095fabb35ca2bdc
Cady <machkhachduy.030811@gmail.com>
2024-10-28 13:21:43 +0700

    Update3

--- QLKH/settings.py
+++ QLKH/settings.py
@@ -138,3 +138,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 django_heroku.settings(locals())
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  
+]
